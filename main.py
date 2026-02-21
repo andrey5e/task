0422274b871917b5a1de72f04dfb4fa0af012ef9
andrey5e <161711012+andrey5e@users.noreply.github.com>
2026-02-21 13:08:08 +0300

    start main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import argparse
+import sys
+from tabulate import tabulate
+from read import CSVDataReader
+from analysis import get_analyzer
 
+# Разбор аргументов командной строки
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Анализ макроэкономических данных',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    
+    parser.add_argument(
+        '--files',
+        nargs='+',
+        required=True,
+    )
+    
+    parser.add_argument(
+        '--report',
+        required=True,
+        choices=['average-gdp'],
+    )
+    
+    return parser.parse_args()
