fb017959e35dadf654f2191e04cc8fa901aa11a6
andrey5e <161711012+andrey5e@users.noreply.github.com>
2026-02-21 13:06:38 +0300

    start analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,22 @@
+from collections import defaultdict
+from typing import List, Dict, Any, Optional
+from abc import ABC, abstractmethod
 
+# Класс для анализаторов
+class BaseAnalyzer(ABC):
+    @abstractmethod
+    # Анализ данных
+    def analyze(self, data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        pass
+    
+    @abstractmethod
+    # Возврат названия отчета
+    def get_report_name(self) -> str:
+        pass
+    
+    # Возврат заголовков таблицы для вывода
+    def get_headers(self) -> List[str]:
+        return ["Страна", "Значение"]
+
+class GDPAverageAnalyzer(BaseAnalyzer):
+    # Анализатор для расчета среднего ВВП по странам
